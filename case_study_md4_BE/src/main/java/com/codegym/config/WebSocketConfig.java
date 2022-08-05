c7e2d9158250226d88c389e3f60fc4533e4f35f5
hantoi0104 <hantoiltpt@gmail.com>
2022-08-05 16:03:34 +0700

    nhăn tin

--- case_study_md4_BE/src/main/java/com/codegym/config/WebSocketConfig.java
+++ case_study_md4_BE/src/main/java/com/codegym/config/WebSocketConfig.java
@@ -7,18 +7,18 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-    @EnableWebSocketMessageBroker
-    public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-        @Override
-        public void registerStompEndpoints(StompEndpointRegistry registry) {
-            registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
-        }
-
-        @Override
-        public void configureMessageBroker(MessageBrokerRegistry registry) {
-            registry.setApplicationDestinationPrefixes("/app");
-            registry.enableSimpleBroker("/chatroom","/user");
-            registry.setUserDestinationPrefix("/user");
-        }
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/socket").setAllowedOriginPatterns("*").withSockJS();
     }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/chatroom","/user");
+        registry.setUserDestinationPrefix("/user");
+    }
+}
 
--- case_study_md4_BE/src/main/java/com/codegym/controller/ChatController.java
+++ case_study_md4_BE/src/main/java/com/codegym/controller/ChatController.java
@@ -2,26 +2,27 @@ package com.codegym.controller;
 
 import com.codegym.models.Message;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class ChatController {
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
-    @MessageMapping("/message")
+    @MessageMapping("/hello")
     @SendTo("/chatroom/public")
-    public Message receiveMessage(@Payload Message message){
+    public Message receiveMessage(@Payload Message message) {
         return message;
     }
 
     @MessageMapping("/private-message")
     public Message recMessage(@Payload Message message){
-        simpMessagingTemplate.convertAndSendToUser(message.getReceiver().getFullName(),"/private",message);
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiver().getEmail(),"/private",message);
         System.out.println(message.toString());
         return message;
     }
